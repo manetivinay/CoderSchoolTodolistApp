8ff66a6dfac2abd0ba40b02615822240ec3eb05a
Vinay Maneti <vinaymaneti@gmail.com>
2016-09-11 16:04:32 +0700

    first commit, with splash screen Login and Registration screen UI

---
+++ app/src/main/java/com/vinaymaneti/coderschooltodolistapp/SplashScreenActivity.java
@@ -0,0 +1,25 @@
+package com.vinaymaneti.coderschooltodolistapp;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    public static final int SPLASH_TIME_OUT = 1000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent loginIntent = new Intent(SplashScreenActivity.this, LoginActivity.class);
+                startActivity(loginIntent);
+            }
+        }, SPLASH_TIME_OUT);
+    }
+}
